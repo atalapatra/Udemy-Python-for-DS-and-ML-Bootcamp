ae35f3e310e7b35773abc08f64e7ebbbe4c9e4e1
Amit Talapatra <amit.talapatra@gmail.com>
2020-06-24 15:41:52 -0400

    Linear Regression Exercises

---
+++ Linear Regression.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+USAhousing = pd.read_csv('Refactored_Py_DS_ML_Bootcamp-master/11-Linear-Regression/USA_Housing.csv')
+
+USAhousing.head()
+
+USAhousing.info()
+
+USAhousing.describe()
+
