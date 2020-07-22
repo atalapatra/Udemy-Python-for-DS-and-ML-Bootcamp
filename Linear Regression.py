339df3cc3ea7b694224aea2c5ad245883c11f204
Amit Talapatra <amit.talapatra@gmail.com>
2020-07-22 19:36:35 -0400

    2020-07-22

--- Linear Regression.py
+++ Linear Regression.py
@@ -12,3 +12,17 @@ USAhousing.info()
 USAhousing.describe()
 
 USAhousing.columns
+
+sns.pairplot(USAhousing)
+
+sns.distplot(USAhousing['Price'])
+
+sns.heatmap(USAhousing.corr())
+
+X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms', 'Avg. Area Number of Bedrooms', 'Area Population']]
+
+y = USAhousing['Price']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
