431239398f06616f3b492dd4bb03378bd5fec253
Amit Talapatra <amit.talapatra@gmail.com>
2020-07-29 12:51:12 -0400

    Changes to Linear Regression.py

--- Linear Regression.py
+++ Linear Regression.py
@@ -11,3 +11,34 @@ USAhousing.info()
 
 USAhousing.describe()
 
+USAhousing.columns
+
+sns.pairplot(USAhousing)
+
+sns.distplot(USAhousing['Price'])
+
+sns.heatmap(USAhousing.corr())
+
+X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+               'Avg. Area Number of Bedrooms', 'Area Population']]
+y = USAhousing['Price']
+
+from sklearn.model_selection import train_test_split
+
+help(train_test_split)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+
+from sklearn.linear_model import LinearRegression
+
+lm = LinearRegression()
+
+lm.fit(X_train,y_train)
+
+# print the intercept
+print(lm.intercept_)
+
+coeff_df = pd.DataFrame(lm.coef_,X.columns,columns=['Coefficient'])
+coeff_df
+
+predictions = lm.predict(X_test)
